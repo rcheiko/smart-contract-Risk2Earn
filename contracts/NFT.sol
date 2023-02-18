7da62e79bee188dc9c1fd04494eb59ce75db1960
Shokker <rcheiko@student.42nice.fr>
2023-02-18 20:17:27 +0100

    started the contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+//@author : rcheiko
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./ERC721A.sol";
+
+contract RiskNftERC721A is Ownable, ERC721A {
+
+    using Strings for uint;
+
+    enum Step {
+        Before,
+        WhitelistSale,
+        PublicSale,
+        SoldOut,
+        Reveal
+    }
+
+    string public baseURI;
+
+    Step public sellingStep;
+    uint private constant MAX_SUPPLY = 10000;
+    uint private constant MAX_WHITELIST = 1000;
+    uint private constant MAX_PUBLIC = 8900;
+    uint private constant MAX_GIFT = 100;
+
+    uint public wlSalePrice = 3 ether; // 3 USDC
+    uint public publicSalePrice = 5 ether; // 5 matic
+
+    bytes32 public merkleRoot;
+
+    // sale start time
+
+    mapping(address => uint) public amountNFTsPerWalletWhitelistSale;
+    mapping(address => uint) public amountNFTsPerWalletPublicSale;
+
+    address private team;
+
+    constructor(address memory _team, bytes _merklesRoot, string memory _baseURI) ERC721A("R2E", "R2E") {
+        merkleRoot = _merklesRoot;
+        baseURI = _baseURI;
+        team = team;
+    }
+
+    modifier callerIsUser() {
+        require(msg.sender == tx.origin, "The caller is not user");
+        _;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function isWhiteListed(address _account, bytes32[] calldata _proof) internal view returns(bool) {
+        return _verify(leaf(_account), _proof);
+    }
+
+    function leaf(address _account) internal pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_account));
+    }
+
+    function _verify(bytes32 _leaf, bytes32[] calldata _proof) internal view returns(bool) {
+        return MerkleProof.verify(_proof, merkleRoot, _leaf);
+    }
+
+    function setStep(uint _step) external onlyOwner {
+        sellingStep = Step(_step);
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return string(abi.encodePacked(baseURI, _tokenId.toString(), ".json"));
+    }
+
+    function whitelistMint(uint _quantity, bytes32[] calldata _proof) external payable callerIsUser {
+        uint price = wlSalePrice * _quantity;
+        require(price != 0, "Price is zero");
+        require(sellingStep == Step.WhitelistSale, "Whitelist sale is not started");
+        require(isWhiteListed(msg.sender, _proof), "You are not whitelisted");
+        require(amountNFTsPerWalletWhitelistSale[msg.sender] + _quantity <= 3, "You can buy max 3 NFTs on the whitelist sale");
+        require(totalSupply() + _quantity <= MAX_WHITELIST, "Whitelist sale is sold out");
+        require(msg.value == price, "Incorrect price");
+        amountNFTsPerWalletWhitelistSale[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function publicSaleMint(uint _quantity) external payable callIsUser {
+        uint price = publicSalePrice * _quantity;
+        require(price != 0, "Price is zero");
+        require(sellingStep == Step.PublicSale, "Public sale is not started");
+        require(amountNFTsPerWalletPublicSale[msg.sender] + _quantity <= 10, "You can buy max 10 NFTs on the public sale");
+        require(totalSupply() + _quantity <= MAX_SUPPLY, "Public sale is sold out");
+        require(msg.value == price, "Incorrect price");
+        amountNFTsPerWalletPublicSale[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+}
